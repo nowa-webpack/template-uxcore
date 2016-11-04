6f2401971b0160e9da17914a2ccd88f83d980e0f
wenzhao.fw <wenzhao.fw@alibaba-inc.com>
2016-11-04 18:03:23 +0800

    dev

--- proj/src/app/routes.jsx
+++ proj/src/app/routes.jsx
@@ -5,30 +5,29 @@ const { React, ReactRouter, ReactDOM } = window;
 // `ReactRouter`文档请看  https://github.com/ReactTraining/react-router/tree/v2.8.1
 const { Router, Link, hashHistory } = ReactRouter;
 
-const App = ({ children, location, routes }) => {
-  return (
-    <div>
-      <Menu mode="horizontal" selectedKeys={routes[routes.length - 1].title}>
-        <Menu.Item key="home">
-          <Link to={'/home'} >首页</Link>
-        </Menu.Item>
-        <Menu.Item key="demo">
-          <Link to={'/demo'} >DEMO</Link>
-        </Menu.Item>
-        <Menu.Item key="error">
-          <Link to={`/${Math.random().toString(32).slice(2)}`} >错误页面</Link>
-        </Menu.Item>
-      </Menu>
-      <div className="kuma-container kuma-container-1180">
-        <ReactCSSTransitionGroup transitionName="route" transitionEnterTimeout={500} transitionLeaveTimeout={100}>
-          {React.cloneElement(children || 'div', {
-            key: location.pathname,
-          })}
-        </ReactCSSTransitionGroup>
-      </div>
+const App = ({ children, location, routes }) => (
+  <div>
+    <Menu mode="horizontal" selectedKeys={routes[routes.length - 1].title}>
+      <Menu.Item key="home">
+        <Link to={'/home'} >首页</Link>
+      </Menu.Item>
+      <Menu.Item key="demo">
+        <Link to={'/demo'} >DEMO</Link>
+      </Menu.Item>
+      <Menu.Item key="error">
+        <Link to={`/${Math.random().toString(32).slice(2)}`} >错误页面</Link>
+      </Menu.Item>
+    </Menu>
+    <div className="kuma-container kuma-container-1180">
+      <ReactCSSTransitionGroup transitionName="route" transitionEnterTimeout={500} transitionLeaveTimeout={100}>
+        {React.cloneElement(children || 'div', {
+          key: location.pathname,
+        })}
+      </ReactCSSTransitionGroup>
     </div>
-  );
-};
+  </div>
+);
+
 
 // 同样匹配`/users/123/portfolios/345`
 // `this.props.params`是`{userId: '123', portfolioId: 345}`
